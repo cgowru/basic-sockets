8e697690e4e3ca8f1d05a8f3d2100565891ff2ae
Chandra Sekhar <cgowru@gmail.com>
2016-01-26 22:38:03 +0530

    No auto populate of items on socket return

--- public/js/app.js
+++ public/js/app.js
@@ -1,16 +1,73 @@
 angular
 .module('myApp',['ngRoute'])
-.factory('$soc',function(){
-	var socket = io();
+.factory('chatService',[
+  	function() {
 
-	return socket;
-})
-.factory('shareable',function(){
-	var  shareable={
-		users:[]
-	};
-	return shareable;
-})
+		var chat = {
+			users:[],
+			items:[],
+			user:{}
+		};
+
+		chat.setUserRoom = function(user) {
+			chat.user = user;
+		}
+
+		chat.setItem = function(item) {
+			chat.items.push(item);
+			chat.items = chat.items.splice(0);
+		}
+
+		chat.getItems = function(items) {
+			return chat[items];
+		}
+
+		chat.setUser = function(user) {
+			chat.users.push(user);
+		}
+
+		var socket = io();
+
+		socket.on('connect',function(data){
+			console.log('Client Connected to Server');
+		});
+
+		socket.on('userJoined',function(user){
+			chat.setUser(user);
+		});
+
+		socket.on('message',function(data){
+			console.log('message listen : ',data);
+			var timestamp = moment.utc(data.timestamp);
+			chat.setItem({
+				text :data.text,
+				timestamp : timestamp.local().format('h:mm a')
+			});	
+		});
+
+		chat.emitUser = function(user){
+			chat.setUserRoom(user);
+
+			socket.emit('user',{
+				timestamp : moment().valueOf(),
+				name: user.name,
+				room: user.room
+			});
+		}
+
+		chat.emitMessage = function(msg) {
+			var item = {
+				text: msg.text,
+				timestamp : moment().valueOf()				
+			}
+			//chat.setItem(item);
+			socket.emit('message',item);
+		}
+
+
+		return chat;
+	}
+])
 .config(function($routeProvider){
 	$routeProvider
 	.when('/',{
@@ -23,55 +80,46 @@ angular
 	});
 })
 .controller('loginCtrl',[
-	'$scope','$window','$soc','shareable','$q',
-	function($scope,$window,socket,shareable,$q
-		) {
+	'$scope','$window','chatService',
+	function($scope,$window,onChat) {
 		$scope.user={};
-
-		socket.on('connect',function(data){
-			console.log('Client Conzanected to Server');
-		});
 
 		$scope.signinsubmit = function ($event){
 			$event.preventDefault();
-				socket.on('user',function(user){
-					shareable.users.push(user);
-					console.log(user);
-				});
 
-				socket.emit('user',{
-					timestamp : moment().valueOf(),
-					username: $scope.user.name,
-					room : $scope.user.room
-				});
-				$window.location.href='/#chat';			
+			onChat.emitUser({
+				name : $scope.user.name,
+				room : $scope.user.room
+			})
+
+			$window.location.href='/#chat';			
 		}
 	}
 ])
 .controller('chatCtrl',[
-	'$scope','$soc','shareable',
-	function($scope,socket,shareable){				
+	'$scope','chatService',
+	function($scope,onChat){				
+		$scope.$watch(
+			function(){
+				return onChat.getItems('items');
+			},
+			function(newval,oldval){
+				if (newval !== oldval) {
+					console.log(newval);
+				   $scope.items = newval;
+				}
+			}
+		);
 
-		$scope.items = [];
-		$scope.username=shareable.users[0].username;
-
-		socket.on('message',function(data){
-			var timestamp = moment.utc(data.timestamp);
-			$scope.$apply(function(){
-				$scope.items.push({
-					text :data.text,
-					timestamp : timestamp.local().format('h:mm a')
-				});	
-			});
-		});
+		$scope.username=onChat.users[0].name;
 
 		$scope.formsubmit = function ($event){
 			$event.preventDefault();
 
-			socket.emit('message',{
-				text: $scope.usermsg,
-				timestamp : moment().valueOf()
+			onChat.emitMessage({
+				text: $scope.usermsg
 			});
+
 			$scope.usermsg='';
 		}
 	}
--- server.js
+++ server.js
@@ -10,39 +10,45 @@ var io = require('socket.io')(http);
 app.use(express.static(__dirname + '/public'));
 
 
-var clientInfo ={
+var clientInfo =[
 
-};
+];
+
+
 
 
 io.on('connection',function(socket) {
 
-	socket.on('user',function(req){
-		clientInfo[socket.id] = req;
-		console.log(req,socket.id);
+	socket.on('user',function(user){
+		clientInfo[socket.id] = user;
+		console.log(user,socket.id,clientInfo);
 
-		//socket.join(req.room);
-		socket.
-		//broadcast.
-		//to(req.room).
-		emit('user',{
-			username : req.username,
-			text : req.username + 'has joined',
+		socket.emit('userJoined',{
+			name : user.name
+		})
+
+		socket
+		.join(user.room,function(err){
+			if (err) {
+				console.log('Failed to join the room');
+				return;
+			}
+			console.log('Successfully joined the room');
+		})
+		.broadcast
+		.to(user.room)
+		.emit('message',{
+			name : user.name,
+			text : user.name + ' has joined',
 			timestamp : moment.valueOf()
 		});
 
 	})
 
-	socket.emit('message',{
-		text:'Welcome',
-		timestamp : moment().valueOf(),
-		username:socket.username
-	})
-	
 	socket.on('message',function(data){
 		console.log(data.text);
 		io.
-		//to(clientInfo[socket.id].room).
+		to(clientInfo[socket.id].room).
 		emit('message',data);
 	})
 
